e443cb0b6cebe191ff4373d9082e5e8c35345a4a
hkgnp <ben@pngs.cc>
2022-11-05 23:19:10 +0800

    fixed 'MMM' issue

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ export const getDateForPage = (d: Date, preferredDateFormat: string) => {
       yyyy: getYear,
       do: getOrdinalNum(getDate),
       MMMM: getMonthInFull,
-      MMM: getMonthInFull,
+      MMM: getMonthInFull.slice(0, 4),
     };
     let dateStr = preferredDateFormat;
     dateStr = dateStr.replace(/yyyy|do|MMM|MMMM/gi, function (matched) {
